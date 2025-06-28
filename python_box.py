5c826eb59acfaebcc7eba6da9638f80c14e972ad
NOKIAO <62821977+luojiaaoo@users.noreply.github.com>
2025-06-29 00:35:14 +0800

    Update python_box.py

--- python_box.py
+++ python_box.py
@@ -54,3 +54,61 @@ for _ in range(1000):
         raise ValueError(f"Error: {a} != {b}, list_uint8={list_uint8}")
     else:
         print(f"Success: {a} == {b}, list_uint8={list_uint8}")
+
+"""
+异常重试装饰器
+"""
+from functools import wraps
+from time import sleep
+from typing import Tuple
+
+
+def retry(exceptions: Tuple[Exception], retries: int = 3, delay: float = 1):
+    """
+    函数执行失败时，重试
+
+    :param retries: 最大重试的次数
+    :param delay: 每次重试的间隔时间，单位 秒
+    :return:
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # 第一次正常执行不算重试次数，所以retries+1
+            for i in range(retries + 1):
+                if i == retries:
+                    return func(*args, **kwargs)
+                else:
+                    try:
+                        return func(*args, **kwargs)
+                    except exceptions as e:
+                        sleep(delay)
+
+        return wrapper
+
+    return decorator
+
+
+"""
+每60秒瞬间切换大小写
+保证电脑不休眠线程
+"""
+import os
+import threading
+import time
+import win32api
+import win32con
+
+def start_thread_to_avoid_sleep_windows():
+    th = threading.Thread(target=mouse_move)
+    th.setDaemon(True)
+    th.start()
+
+def mouse_move():
+    while True:
+        win32api.keybd_event(0x14, 0, 0, 0)
+        win32api.keybd_event(0x14, 0, win32con.KEYEVENTF_KEYUP, 0)
+        win32api.keybd_event(0x14, 0, 0, 0)
+        win32api.keybd_event(0x14, 0, win32con.KEYEVENTF_KEYUP, 0)
+        time.sleep(60)
